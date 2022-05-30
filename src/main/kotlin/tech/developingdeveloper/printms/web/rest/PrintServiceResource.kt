4ef3aec34f766eb80e27fc66ff01c714e0f95865
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-05-31 03:35:45 +0530

    Adding POST method to print pdf.

--- src/main/kotlin/tech/developingdeveloper/printms/web/rest/PrintServiceResource.kt
+++ src/main/kotlin/tech/developingdeveloper/printms/web/rest/PrintServiceResource.kt
@@ -3,14 +3,19 @@ package tech.developingdeveloper.printms.web.rest
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import tech.developingdeveloper.printms.services.FileService
 import tech.developingdeveloper.printms.services.PrinterService
 import tech.developingdeveloper.printms.services.dtos.PrinterDTO
 
 
 @RestController
 class PrintServiceResource(
-    private val printerService: PrinterService
+    private val printerService: PrinterService,
+    private val fileService: FileService
 ) {
 
     @GetMapping("/")
@@ -34,4 +39,14 @@ class PrintServiceResource(
             printerService.getPrinter(printerName)
         return ResponseEntity.ok(body)
     }
+
+    @PostMapping("/print")
+    fun printPdf(
+        @RequestParam file: MultipartFile,
+        @RequestParam printerName: String
+    ) {
+        val tempFilePath = fileService.saveFile(file, null)
+        printerService.printPdf(tempFilePath, printerName)
+    }
+
 }
