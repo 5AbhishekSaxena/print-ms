847a4d1eb8c9907e01d96cd9d7374a233c387fcd
Abhishek Saxena <5abhisheksaxena@gmail.com>
2022-05-30 16:27:30 +0530

    Optimize imports.

--- src/main/kotlin/tech/developingdeveloper/printms/PrintMsApplication.kt
+++ src/main/kotlin/tech/developingdeveloper/printms/PrintMsApplication.kt
@@ -1,10 +1,7 @@
 package tech.developingdeveloper.printms
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import tech.developingdeveloper.printms.services.impl.PrinterServiceImpl
 
 @SpringBootApplication
 class PrintMsApplication
