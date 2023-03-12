1dc5443d703e4d1806c2b422a6010175490a60f4
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-03-13 03:01:51 +0530

    Rename ExceptionHandler to ResourceAdvice.

--- src/main/kotlin/tech/developingdeveloper/printms/web/rest/utils/exceptions/ResourceAdvice.kt
+++ src/main/kotlin/tech/developingdeveloper/printms/web/rest/utils/exceptions/ResourceAdvice.kt
@@ -1,16 +1,17 @@
 package tech.developingdeveloper.printms.web.rest.utils.exceptions
 
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class ExceptionHandler(
+class ResourceAdvice(
     private val apiErrorFactory: ApiErrorFactory
 ) {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
-    fun generalExceptionHandler(excpetion: Exception): ResponseEntity<ApiError> {
-        val apiError = apiErrorFactory.create(exception = excpetion)
+    @ExceptionHandler(Exception::class)
+    fun generalExceptionHandler(exception: Exception): ResponseEntity<ApiError> {
+        val apiError = apiErrorFactory.create(exception = exception)
         return ResponseEntity(apiError, apiError.status)
     }
 }
